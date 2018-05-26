f363a3e85241a8a4ffa90ada5191ea05e0272c26
Ezia <esia.belbachir@protonmail.com>
2018-05-26 19:36:35 -0400

    Revert "Merge branch 'master' of https://github.com/MrFouss/8INF840-TP1"
    This reverts commit 58bb15ea722d304872c629849715272edf042147, reversing
    changes made to 7f8acc49cd243ca9887e3cd1f108c4df3fde2936.

--- Exercise1/params.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// size of the initial stack
-const int DECK_SIZE = 100;
-
-// true for the complete details of every stack ; false for a condensed view 
-const bool LOG_DETAILS = false;
--- Exercise4/EyeColor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-enum EyeColor { none=-1, blue = 0, green = 1, brown = 2, black = 3 };
-
-std::string eyeColorToString(EyeColor eye);
-EyeColor itoEyeColor(int i);
