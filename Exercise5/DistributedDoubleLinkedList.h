e810518f6746127915a143d777d91f8e4b5824ee
GautSlayer <gauthierberod@gmail.com>
2018-05-27 08:01:04 +0200

    Test avec des threads et pipe
    je push pour dire de pas perde ce que j'ai mis je sais pas combien de temps à faire, même si ca marche pas vraiment...

--- Exercise5/DistributedDoubleLinkedList.h
+++ Exercise5/DistributedDoubleLinkedList.h
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <thread>
+#include <stdio.h>
+#include <Windows.h>
+
+#define BUFSIZE 100
 
 using namespace std;
 
@@ -12,7 +17,20 @@ private:
 		int id;
 		Thread* next;
 		Thread* prev;
-		Thread(const int& data_item, Thread* next_ptr = 0, Thread* prev_ptr = 0) : id(data_item), next(next_ptr), prev(prev_ptr) {}
+		Thread(const int& data_item, Thread* next_ptr = 0, Thread* prev_ptr = 0) : id(data_item), next(next_ptr), prev(prev_ptr) 
+		{
+			stopRun = false;
+			getIdRequest = false;
+			SECURITY_ATTRIBUTES saAttr;
+			CreatePipe(hReadPipe, hWritePipe,&saAttr, 0);
+			thread myThread(&Thread::run, this);
+			myThread.detach();
+		}
+		~Thread()
+		{
+			StopThread();
+		}
+
 		inline int SommeIds()
 		{
 			cout << "Thread's Id : " << id << " asked for ids sum" << endl;
@@ -23,6 +41,15 @@ private:
 				<< sommeIds <<" = " << leftSum <<" (left sum) + " << id <<" (my id) + " << rightSum <<" (right sum)"
 				<< endl << endl << endl;
 			return sommeIds;
+		}
+
+		inline void StopThread() {
+			stopRun = true;
+		}
+
+		inline int getThreadId() {
+			getIdRequest = true;
+			return ReadFromPipe();
 		}
 
 	private:
@@ -64,6 +91,62 @@ private:
 				cout << "Thread's Id : " << id << " right thread Id sum = " << valueToReturn <<" = " << nextId << " (following's Id) + " << rightSum << " (following's right ids sum)" << endl;
 			}
 			return valueToReturn;
+		}
+
+		thread myThread;
+		bool stopRun; //bool to stop the thread
+		bool getIdRequest; //bool to say to the thread that we need to get its Id;
+		PHANDLE hReadPipe = NULL;
+		PHANDLE hWritePipe = NULL;
+
+		inline void run(){
+			while (!stopRun)
+			{
+				if (getIdRequest)
+				{
+					cout << "Thread id :" << this_thread::get_id() << endl;
+					//WriteToPipe((CHAR*)id);
+					int i = 69;
+					WriteToPipe(&i);
+				}
+			}
+		}
+
+		inline void WriteToPipe(int* ch)
+		{
+			DWORD dwRead = 100, dwWritten;
+			//CHAR chBuf[BUFSIZE];
+			getIdRequest = false;
+			bool succes = true;
+			for (;;)
+			{
+				succes = WriteFile(hWritePipe, ch, dwRead, &dwWritten, NULL);
+				if (!succes)
+				{
+					break;
+				}
+			}
+			CloseHandle(hWritePipe);
+		}
+
+		inline int ReadFromPipe()
+		{
+			DWORD dwRead, dwWritten;
+			//CHAR chBuf[BUFSIZE];
+			//ReadFile(hReadPipe, chBuf, BUFSIZE, &dwRead, NULL);
+			//return chBuf;
+
+			int intBuf[BUFSIZE];
+			bool succes = true;
+			for (;;)
+			{
+				succes = ReadFile(hReadPipe, intBuf, BUFSIZE, &dwRead, NULL);
+				if (!succes)
+				{
+					break;
+				}
+			}
+			return intBuf[0];
 		}
 	};
 
--- Exercise5/main.cpp
+++ Exercise5/main.cpp
@@ -40,8 +40,8 @@ int main()
 	cout << "\n";
 	*/
 
-	
-	//Test Distributed Double Chained List
+	/*
+	//Test Distributed Double Chained List without Threading
 	DistributedDoubleLinkedList* ddcl = new DistributedDoubleLinkedList();
 	ddcl->add(2);
 	ddcl->add(7);
@@ -55,7 +55,12 @@ int main()
 	ddcl->start->next->SommeIds();
 	ddcl->end->SommeIds();
 	ddcl->end->prev->prev->SommeIds();
-	
+	*/
+
+	//Test Distributed Double Chained List with Threading
+	DistributedDoubleLinkedList* ddcl = new DistributedDoubleLinkedList();
+	ddcl->add(2);
+	cout <<"getThreadID() :" << ddcl->start->getThreadId()<< endl;
 
 	cout << "\n";
 	system("pause");
